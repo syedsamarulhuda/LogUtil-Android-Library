77dca647b71ae7fe824ef37fce38a6c772cb1c6c
Syed Samarul Huda <syed.samarul.huda@flyrobe.com>
2017-05-19 19:06:07 +0530

    Adding fetaure

--- app/src/main/java/com/samar/logexample/MainActivity.java
+++ app/src/main/java/com/samar/logexample/MainActivity.java
@@ -3,11 +3,15 @@ package com.samar.logexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.samar.logutil.LogUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogUtil.d("##SAMAR","TESTING");
     }
 }
--- logutil/src/main/java/com/samar/logutil/LogUtil.java
+++ logutil/src/main/java/com/samar/logutil/LogUtil.java
@@ -6,21 +6,20 @@ package com.samar.logutil;
 
 public class LogUtil {
 
-    private static final boolean isDebug = BuildConfig.BUILD_TYPE.equalsIgnoreCase("debug");
 
-    public static void i(String tag, String string) {
+    public static void i(boolean isDebug,String tag, String string) {
         if (isDebug) android.util.Log.i(tag, string);
     }
-    public static void e(String tag, String string) {
+    public static void e(boolean isDebug,String tag, String string) {
         if (isDebug) android.util.Log.e(tag, string);
     }
-    public static void d(String tag, String string) {
+    public static void d(boolean isDebug,String tag, String string) {
         if (isDebug) android.util.Log.d(tag, string);
     }
-    public static void v(String tag, String string) {
+    public static void v(boolean isDebug,String tag, String string) {
         if (isDebug) android.util.Log.v(tag, string);
     }
-    public static void w(String tag, String string) {
+    public static void w(boolean isDebug,String tag, String string) {
         if (isDebug) android.util.Log.w(tag, string);
     }
 }
